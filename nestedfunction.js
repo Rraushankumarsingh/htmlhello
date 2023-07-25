5e7acd55f8645abcc644264e2ee68b4435815473
Rraushankumarsingh <sraushan191@gmail.com>
2023-07-25 19:43:33 +0530

    first

---
+++ nestedfunction.js
@@ -0,0 +1,13 @@
+function addsquare(a,b)
+{
+    let ab=square(a);
+    let ad=square(b);
+     
+    function square(num)
+    { 
+       return num*num;
+    }
+    return ab+ad;
+}
+
+document.write(addsquare(3,4));
